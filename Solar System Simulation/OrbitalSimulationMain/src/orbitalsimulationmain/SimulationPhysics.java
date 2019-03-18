a56a4c1278b4d3f8fa940b5d271530ff141bff4a
Cicillo <frankiebalassone@gmail.com>
2019-03-18 15:21:33 -0400

    shit commit (added a tiny line of code)

--- Solar System Simulation/OrbitalSimulationMain/src/orbitalsimulationmain/SimulationPhysics.java
+++ Solar System Simulation/OrbitalSimulationMain/src/orbitalsimulationmain/SimulationPhysics.java
@@ -17,20 +17,21 @@ public class SimulationPhysics {
 	/*
 	This method handles the application of Newton's Law of Universal Gravitation to the celestial bodies' velocity and position vectors.
 	*/
-	// public Vector2D applyNewtonsLaw(Vector2D Force) {
-		
-		//Vector2D position = new Vector2D(,);
-	//	return;
-	//}
-	
+	public Vector2D applyGravitationalForce(CelestialBody cb) {
+		CelestialBody otherBody = new CelestialBody(0,0,0,0,0);
+		cb.setTangentialVelocity(newtonsLaw(cb.getMass(),otherBody.getMass(), ));
+		Vector2D position = new Vector2D(,);
+		return;
+	}
+
 	/*
 	 This method applies Newton's law based on the interactions between two bodies
 	 */
-	public Vector2D newtonsLaw(double mass1, double mass2, double distanceX, double distanceY, CelestialBody firstBody, CelestialBody otherBody) {
+	public double newtonsLaw(double mass1, double mass2, double distanceX, double distanceY, CelestialBody firstBody, CelestialBody otherBody) {
 		distanceX = firstBody.getxCoordinate() - otherBody.getxCoordinate();
 		distanceY = firstBody.getyCoordinate() - otherBody.getyCoordinate();
 		double distanceTotal = Math.hypot(distanceX, distanceY) - firstBody.getRadius() - otherBody.getRadius(); //gets the total distance magnitude between the 2 circles
 		double vectorScalar = (SimulationConstants.UNIV_GRAV_CONSTANT * firstBody.getMass() * otherBody.getMass())/distanceTotal;
-		return; //TO DO
+		return vectorScalar; //TO DO
 	} 
 }
