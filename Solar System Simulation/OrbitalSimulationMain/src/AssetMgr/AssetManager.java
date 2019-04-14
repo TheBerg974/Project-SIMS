7d0efdf6484861cc57df0e09f4619f3c46063ffb
Cicillo <frankiebalassone@gmail.com>
2019-04-14 13:59:36 -0400

    Tiny commit - just added more assets and am trying to beautify the program

--- Solar System Simulation/OrbitalSimulationMain/src/AssetMgr/AssetManager.java
+++ Solar System Simulation/OrbitalSimulationMain/src/AssetMgr/AssetManager.java
@@ -20,95 +20,130 @@ import javafx.scene.paint.ImagePattern;
  */
 public class AssetManager {
 
-    private static Background backgroundGif = null;
+	private static Background backgroundGif = null;
 
-    private static ImagePattern earthImage;
+	private static ImagePattern earthImage;
 
-    private static ImagePattern neptuneImage;
+	private static ImagePattern neptuneImage;
 
-    private static ImagePattern sunImage;
-    
-    private static ImagePattern starImage;
-    
-    private static ImagePattern moonImage;
+	private static ImagePattern sunImage;
 
-    private static ImagePattern marsImage;
-    
-    private static ImagePattern asteroidImage;
-    
-    private static ImagePattern michaelBayExplosionImage;
-    
-    private static ImagePattern michaelBayPointingImage;
-    
-    private static ImagePattern michaelBayGangsterImage;
+	private static ImagePattern starImage;
 
-    private static Image background;
-    
-    private static Image collisionGif;
-    
-    private static Image runAwayGif;
+	private static ImagePattern moonImage;
 
-    private static String fileURL(String relativePath) {
-        return new File(relativePath).toURI().toString();
-    }
-    /**
-     * Loads all of the assets needed for this program to look presentable
-     */
-    public static void preloadAllAssets() {
-        background = new Image(fileURL("./assets/gifs/background.gif"));
+	private static ImagePattern marsImage;
 
-        backgroundGif = new Background(new BackgroundImage(background, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT));
-        
-        //Michael Bay Mode gifs
-        collisionGif = new Image(fileURL("./assets/gifs/collision.gif"));        
-        runAwayGif = new Image(fileURL("./assets/gifs/run_away.gif"));
-        
-        //Regular Simulation Images        
-        earthImage = new ImagePattern(new Image(fileURL("./assets/images/earth.png")));
-        neptuneImage = new ImagePattern(new Image(fileURL("./assets/images/neptune.png")));
-        sunImage = new ImagePattern(new Image(fileURL("./assets/images/sun.png")));    
-        starImage = new ImagePattern(new Image(fileURL("./assets/images/star.png")));
-        marsImage = new ImagePattern(new Image(fileURL("./assets/images/mars.png")));   
-        moonImage = new ImagePattern(new Image(fileURL("./assets/images/moon.png")));        
-        asteroidImage = new ImagePattern(new Image(fileURL("./assets/images/asteroid.png")));
-        
-        //Michael Bay mode images
-        michaelBayExplosionImage = new ImagePattern(new Image(fileURL("./assets/images/bay_explosion.png")));        
-        michaelBayPointingImage = new ImagePattern(new Image(fileURL("./assets/images/bay_pointing.png")));        
-        michaelBayGangsterImage = new ImagePattern(new Image(fileURL("./assets/images/bay_gangster.png")));
-    }
+	private static ImagePattern asteroidImage;
 
-    public static Background getBackgroundImage() {
-        BackgroundSize backgroundSize = new BackgroundSize(1300, 900, false, false, false, false);
-        return new Background(new BackgroundImage(background, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize));
-    }
+	private static ImagePattern michaelBayExplosionImage;
 
-    public static ImagePattern getEarthImage() {
-        return earthImage;
-    }
+	private static ImagePattern michaelBayPointingImage;
 
-    public static ImagePattern getNeptuneImage() {
-        return neptuneImage;
-    }
+	private static ImagePattern michaelBayGangsterImage;
 
-    public static ImagePattern getSunImage() {
-        return sunImage;
-    }
+	private static ImagePattern genericExplosionImage;
 
-    public static ImagePattern getStarImage() {
-        return starImage;
-    }
+	private static ImagePattern usaFlagImage;
 
-    public static ImagePattern getMoonImage() {
-        return moonImage;
-    }
+	private static Image background;
 
-    public static ImagePattern getMarsImage() {
-        return marsImage;
-    }
+	private static Image collisionGif;
 
-    public static ImagePattern getAsteroidImage() {
-        return asteroidImage;
-    }
+	private static Image runAwayGif;
+
+	private static String fileURL(String relativePath) {
+		return new File(relativePath).toURI().toString();
+	}
+
+	/**
+	 * Loads all of the assets needed for this program to look presentable
+	 */
+	public static void preloadAllAssets() {
+		background = new Image(fileURL("./assets/gifs/background.gif"));
+
+		backgroundGif = new Background(new BackgroundImage(background, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT));
+
+		//Michael Bay Mode gifs
+		collisionGif = new Image(fileURL("./assets/gifs/collision.gif"));
+		runAwayGif = new Image(fileURL("./assets/gifs/run_away.gif"));
+
+		//Regular Simulation Images        
+		earthImage = new ImagePattern(new Image(fileURL("./assets/images/earth.png")));
+		neptuneImage = new ImagePattern(new Image(fileURL("./assets/images/neptune.png")));
+		sunImage = new ImagePattern(new Image(fileURL("./assets/images/sun.png")));
+		starImage = new ImagePattern(new Image(fileURL("./assets/images/star.png")));
+		marsImage = new ImagePattern(new Image(fileURL("./assets/images/mars.png")));
+		moonImage = new ImagePattern(new Image(fileURL("./assets/images/moon.png")));
+		asteroidImage = new ImagePattern(new Image(fileURL("./assets/images/asteroid.png")));
+
+		//Michael Bay mode images
+		michaelBayExplosionImage = new ImagePattern(new Image(fileURL("./assets/images/bay_explosion.png")));
+		michaelBayPointingImage = new ImagePattern(new Image(fileURL("./assets/images/bay_pointing.png")));
+		michaelBayGangsterImage = new ImagePattern(new Image(fileURL("./assets/images/bay_gangster.png")));
+		genericExplosionImage = new ImagePattern(new Image(fileURL("./assets/images/generic_explosion.png")));
+		usaFlagImage = new ImagePattern(new Image(fileURL("./assets/images/usa_flag.png")));
+	}
+
+	public static Background getBackgroundImage() {
+		BackgroundSize backgroundSize = new BackgroundSize(1300, 900, false, false, false, false);
+		return new Background(new BackgroundImage(background, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize));
+	}
+
+	public static ImagePattern getEarthImage() {
+		return earthImage;
+	}
+
+	public static ImagePattern getNeptuneImage() {
+		return neptuneImage;
+	}
+
+	public static ImagePattern getSunImage() {
+		return sunImage;
+	}
+
+	public static ImagePattern getStarImage() {
+		return starImage;
+	}
+
+	public static ImagePattern getMoonImage() {
+		return moonImage;
+	}
+
+	public static ImagePattern getMarsImage() {
+		return marsImage;
+	}
+
+	public static ImagePattern getAsteroidImage() {
+		return asteroidImage;
+	}
+
+	public static ImagePattern getMichaelBayExplosionImage() {
+		return michaelBayExplosionImage;
+	}
+
+	public static ImagePattern getMichaelBayPointingImage() {
+		return michaelBayPointingImage;
+	}
+
+	public static ImagePattern getMichaelBayGangsterImage() {
+		return michaelBayGangsterImage;
+	}
+
+	public static ImagePattern getGenericExplosionImage() {
+		return genericExplosionImage;
+	}
+
+	public static ImagePattern getUsaFlagImage() {
+		return usaFlagImage;
+	}
+
+	public static Image getCollisionGif() {
+		return collisionGif;
+	}
+
+	public static Image getRunAwayGif() {
+		return runAwayGif;
+	}
 
 }
--- Solar System Simulation/OrbitalSimulationMain/src/orbitalsimulationmain/MainMenuDocumentController.java
+++ Solar System Simulation/OrbitalSimulationMain/src/orbitalsimulationmain/MainMenuDocumentController.java
@@ -5,7 +5,7 @@
  */
 package orbitalsimulationmain;
 
-
+import AssetMgr.AssetManager;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,6 +19,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -28,94 +29,120 @@ import javafx.stage.Stage;
  * @author Frankie
  */
 public class MainMenuDocumentController implements Initializable {
+	
+	Font MICHAEL_BAY_MODE = new Font("Times New Roman", 14);
+	
+	@FXML
+	private Label labelMainMenu;
 
-    @FXML
-    private Label labelMainMenu;
+	@FXML
+	private Button buttonBlankScenario;
 
-    @FXML
-    private Button buttonBlankScenario;
+	@FXML
+	private Button buttonEarthSun;
 
-    @FXML
-    private Button buttonEarthSun;
+	@FXML
+	private Button buttonEarthMoon;
 
-    @FXML
-    private Button buttonEarthMoon;
-    
+	@FXML
+	private Button buttonMichaelBayMode;
 
-    @FXML
-    public void handleButtonBlankScenarioAction(ActionEvent ae) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-            Stage stage = new Stage();
-            Scene scene = new Scene(root);
+	@FXML
+	public void handleButtonBlankScenarioAction(ActionEvent ae) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
 
-            stage.setScene(scene);
-            stage.setTitle("OrbitalSimulator");
-            scene.getRoot().requestFocus();
-            stage.setResizable(false);
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(MainMenuDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+			stage.setScene(scene);
+			stage.setTitle("OrbitalSimulator");
+			scene.getRoot().requestFocus();
+			stage.setResizable(false);
+			stage.show();
+		} catch (IOException ex) {
+			Logger.getLogger(MainMenuDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    @FXML
-    public void handleButtonEarthSunAction(ActionEvent ae) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("EarthSunDocument.fxml"));
-            Stage stage = new Stage();
-            Scene scene = new Scene(root);
+	@FXML
+	public void handleButtonEarthSunAction(ActionEvent ae) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("EarthSunDocument.fxml"));
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
 
-            stage.setScene(scene);
-            stage.setTitle("Orbital Simulator (Earth-Sun system)");
-            scene.getRoot().requestFocus();
-            stage.setResizable(false);
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(MainMenuDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+			stage.setScene(scene);
+			stage.setTitle("Orbital Simulator (Earth-Sun system)");
+			scene.getRoot().requestFocus();
+			stage.setResizable(false);
+			stage.show();
+		} catch (IOException ex) {
+			Logger.getLogger(MainMenuDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    @FXML
-    public void handleButtonEarthMoonAction(ActionEvent ae) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("EarthMoonDocument.fxml"));
-            Stage stage = new Stage();
-            Scene scene = new Scene(root);
+	@FXML
+	public void handleButtonEarthMoonAction(ActionEvent ae) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("EarthMoonDocument.fxml"));
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
 
-            stage.setScene(scene);
-            stage.setTitle("Orbital Simulator (Earth-Moon system)");
-            scene.getRoot().requestFocus();
-            stage.setResizable(false);
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(MainMenuDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    @FXML
-    public void handleButtonMichaelBayModeAction(ActionEvent ae) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("MichaelBayMode.fxml"));
-            Stage stage = new Stage();
-            Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.setTitle("Orbital Simulator (Earth-Moon system)");
+			scene.getRoot().requestFocus();
+			stage.setResizable(false);
+			stage.show();
+		} catch (IOException ex) {
+			Logger.getLogger(MainMenuDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-            stage.setScene(scene);
-            stage.setTitle("MICHAEL BAY MODE!!!");
-            scene.getRoot().requestFocus();
-            stage.setResizable(false);
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(MainMenuDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	@FXML
+	public void handleButtonMichaelBayModeAction(ActionEvent ae) {
+		try {
+			FXMLDocumentController.michaelBayModeEnabled = !FXMLDocumentController.michaelBayModeEnabled;
+			if (FXMLDocumentController.michaelBayModeEnabled) {
+				//Sets a special font and color if Michael Bay Mode is enabled
+				buttonMichaelBayMode.setStyle("-fx-background-color: #FFA500; ");
+				Label labelBayModeYes = new Label();
+				labelBayModeYes.setFont(MICHAEL_BAY_MODE);
+				labelBayModeYes.setText("MICHAEL BAY MODE ENABLED");
+				buttonMichaelBayMode.setText(labelBayModeYes.getText());
+				
+				
+				labelMainMenu.setText(labelBayModeYes.getText());
+				labelBayModeYes.setFont(MICHAEL_BAY_MODE);
+				
+				Parent root = FXMLLoader.load(getClass().getResource("MichaelBayMode.fxml"));
+				Stage stage = new Stage();
+				Scene scene = new Scene(root);
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+				stage.setScene(scene);
+				stage.setTitle("MICHAEL BAY MODE!!!");
+				scene.getRoot().requestFocus();
+				stage.setResizable(false);
+				stage.show();
+			} else {
+				//BackgroundImage usaFlag = new BackgroundImage(new Image(getClass().getResource(AssetManager.getUsaFlagImage().toString()).toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+				//Background usa = new Background(usaFlag);
+				//buttonMichaelBayMode.setGraphic(new ImageView(AssetManager.getUsaFlagImage()));
+				buttonMichaelBayMode.setStyle("-fx-background-color: #F5F5F5; ");
+				buttonMichaelBayMode.setText("Enable Michael Bay Mode");
+				
+				labelMainMenu.setText("Choose what scenario you prefer");
+			}
+		} catch (IOException ex) {
+			Logger.getLogger(MainMenuDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    }
+	/**
+	 * Initializes the controller class.
+	 */
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		buttonMichaelBayMode.setText("Enable Michael Bay Mode");
+	}
 
 }
